bd5d61ff322f29f08357842ff7177713c3c976f1
yasuda <yasuda@192.168.128.102>
2018-05-29 14:23:51 +0900

    next step

--- StringHandle/src/CMain.java
+++ StringHandle/src/CMain.java
@@ -2,7 +2,9 @@
 public class CMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Moodle が出力する完了ステータスの時刻文字列
+		String input = "2017年 11月 1日(水曜日) 20:52";
+		String output = input.replace("月", "/");
 
 	}
 
